07f3d7579c818db25cf78fd90fd4c712d117d920
DamianDzia <91822456+DamianDzia@users.noreply.github.com>
2021-10-11 23:32:12 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(+pier());
+    }
+   static double a=25;
+    public static double pier() {
+
+        return Math.sqrt(a);
+    }
+
+
+}
+
